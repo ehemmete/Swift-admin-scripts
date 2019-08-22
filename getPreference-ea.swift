b94b78df7f18de75182fc369d294bbdbc99c330f
Eric Hemmeter <eric@sneakypockets.com>
2019-08-22 12:12:05 -0500

    Added Usage message

--- getPreference-ea.swift
+++ getPreference-ea.swift
@@ -1,5 +1,13 @@
 #!/usr/bin/swift
 import Foundation
+let argCount = CommandLine.argc
+if argCount != 3 {
+    let commandName = URL(fileURLWithPath: CommandLine.arguments[0]).lastPathComponent
+    print("Usage: \(commandName) key preferenceDomain")
+    print("Example:")
+    print("       \(commandName) orientation com.apple.Dock")
+    exit(1)
+}
 let key = CommandLine.arguments[1] as CFString
 let domain = CommandLine.arguments[2] as CFString
 if let preference = CFPreferencesCopyAppValue(key, domain) {
